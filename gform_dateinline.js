58fff47e4ca31a089c3b21473220eac2882b766e
Patrick Porto <patrick.s.porto@gmail.com>
2015-11-24 17:30:24 -0200

    Adding javascript

---
+++ gform_dateinline.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($) {
+    $( ".dateinline" ).each(function () {
+        $that = $(this);
+        $that.datepicker({
+            altField:  $that.parent().find('input')
+        });
+    })
+});
